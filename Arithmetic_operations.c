5e1028cec22c7f18fd768570c04d260c346448f6
ratnamraju-420 <119720469+ratnamraju-420@users.noreply.github.com>
2022-12-31 22:19:55 +0530

    #include<stdio.h> int add(int a,int b) {     int c;     c=a+b;     return c; } int diff(int a,int b) {     int c;     c=a-b;     return c; } int pro(int a,int b) {     int c;     c=a*b;     return c; } int quo(int a,int b) {     int c;     c=a/b;     return c; } int rem(int a,int b) {     int c;     c=a%b;     return c; } int main() {     int a,b;     scanf("%d%d",&a,&b);     int x=add(a,b);     printf("Sum:%d ",x);     int x2=diff(a,b);     printf("Difference:%d ",x2);     int x3=pro(a,b);     printf("Product:%d ",x3);     int x4=quo(a,b);     printf("Quotient:%d ",x4);     int x5=rem(a,b);     printf("Remainder:%d ",x5); }

--- Arithmetic_operations.c
+++ Arithmetic_operations.c
@@ -1,16 +1,51 @@
 #include<stdio.h>
+int add(int a,int b)
+{
+    int c;
+    c=a+b;
+    return c;
+}
+int diff(int a,int b)
+{
+    int c;
+    c=a-b;
+    return c;
+}
+int pro(int a,int b)
+{
+    int c;
+    c=a*b;
+    return c;
+}
+int quo(int a,int b)
+{
+    int c;
+    c=a/b;
+    return c;
+}
+int rem(int a,int b)
+{
+    int c;
+    c=a%b;
+    return c;
+}
 int main()
 {
-    int a,b,s,d,p,q,r;
+    int a,b;
     scanf("%d%d",&a,&b);
-    s=a+b;
-    d=a-b;
-    p=a*b;
-    q=a/b;
-    r=a%b;
+    int x=add(a,b);
     printf("Sum:%d
-Difference:%d
-Product:%d
-Quotient:%d
-Remainder:%d",s,d,p,q,r);
+",x);
+    int x2=diff(a,b);
+    printf("Difference:%d
+",x2);
+    int x3=pro(a,b);
+    printf("Product:%d
+",x3);
+    int x4=quo(a,b);
+    printf("Quotient:%d
+",x4);
+    int x5=rem(a,b);
+    printf("Remainder:%d
+",x5);
 }
