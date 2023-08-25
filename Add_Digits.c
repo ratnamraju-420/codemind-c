b35a615ed521b2e6d01c571e36ef1812e7c7a4a4
ratnamraju-420 <119720469+ratnamraju-420@users.noreply.github.com>
2023-08-25 15:03:20 +0530

    #include<stdio.h> int main() {     int i,j;     scanf("%d",&i);     j=i%9;     printf("%d",j); }

--- Add_Digits.c
+++ Add_Digits.c
@@ -1,17 +1,8 @@
 #include<stdio.h>
 int main()
 {
-    int n,d,s=0;
-    scanf("%d",&n);
-    while(n>0||s>9)
-    {
-        if(n==0)
-        {
-            n=s;
-            s=0;
-        }
-        s=s+n%10;
-        n=n/10;
-    }
-    printf("%d",s);
+    int i,j;
+    scanf("%d",&i);
+    j=i%9;
+    printf("%d",j);
 }
