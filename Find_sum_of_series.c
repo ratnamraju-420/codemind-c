5a68e23a12a2543580f87c5b877ad98ca8cd2d5b
ratnamraju-420 <119720469+ratnamraju-420@users.noreply.github.com>
2023-09-15 20:13:22 +0530

    #include<stdio.h> int main() {     int n;     float i,s=0.0;     scanf("%d",&n);     for(i=1;i<=n;i++)     {         s=s+1/i;     }     printf("%.2f",s);     return 0; }

--- Find_sum_of_series.c
+++ Find_sum_of_series.c
@@ -1,15 +1,13 @@
 #include<stdio.h>
-float sum(int n)
-{
-    float i,s=0.0;
-    for(i=1;i<=n;i++)
-       s=s+1/i;
-    return s;
-}
 int main()
 {
     int n;
+    float i,s=0.0;
     scanf("%d",&n);
-    printf("%.2f",sum(n));
+    for(i=1;i<=n;i++)
+    {
+        s=s+1/i;
+    }
+    printf("%.2f",s);
     return 0;
 }
