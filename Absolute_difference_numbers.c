a0b288f00fbf867102da232e7b359fa57a7d50cb
ratnamraju-420 <119720469+ratnamraju-420@users.noreply.github.com>
2023-09-17 19:29:23 +0530

    #include<stdio.h> #include<math.h> int main() {     int n,m,d,c=0,arr[100],i=0,temp,s=0,r=0,j;     scanf("%d%d",&n,&m);     temp=n;     while(n>0)     {         d=n%10;         arr[i]=d;         n=n/10;         c++;         i++;     }     for(j=0;j<m;j++)     {         s=(s*10)+arr[j];     }     for(j=i-1;j>i-m-1;j--)     {         r=(r*10)+arr[j];     }     n=0;     while(s)     {         n=(n*10)+(s%10);         s=s/10;     }     printf("%d",abs(n-r));
    }

--- Absolute_difference_numbers.c
+++ Absolute_difference_numbers.c
@@ -2,16 +2,31 @@
 #include<math.h>
 int main()
 {
-	int n,m,a,b,n1,n2,r,q,d,ad;
-	scanf("%d%d",&n,&m);//
-	n1=log10(n)+1;
-	n2=n1-m;
-	a=pow(10,m);
-	b=pow(10,n2);
-	r=n%a;
-	q=n/b;
-	d=r-q;
-	ad=abs(d);
-	printf("%d",ad);
-		
+    int n,m,d,c=0,arr[100],i=0,temp,s=0,r=0,j;
+    scanf("%d%d",&n,&m);
+    temp=n;
+    while(n>0)
+    {
+        d=n%10;
+        arr[i]=d;
+        n=n/10;
+        c++;
+        i++;
+    }
+    for(j=0;j<m;j++)
+    {
+        s=(s*10)+arr[j];
+    }
+    for(j=i-1;j>i-m-1;j--)
+    {
+        r=(r*10)+arr[j];
+    }
+    n=0;
+    while(s)
+    {
+        n=(n*10)+(s%10);
+        s=s/10;
+    }
+    printf("%d",abs(n-r));
+    
 }
