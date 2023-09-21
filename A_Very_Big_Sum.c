83bef685a2036a5aa17d36d9435d6b34b0efb239
ratnamraju-420 <119720469+ratnamraju-420@users.noreply.github.com>
2023-09-21 19:25:27 +0530

    #include<stdio.h> int main() {     long long int n,i,arr[100],sum=0;     scanf("%lld",&n);     for(i=0;i<n;i++)     {         scanf("%lld",&arr[i]);     }     for(i=0;i<n;i++)     {         sum=sum+arr[i];     }     printf("%lld",sum); }

---
+++ A_Very_Big_Sum.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    long long int n,i,arr[100],sum=0;
+    scanf("%lld",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%lld",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        sum=sum+arr[i];
+    }
+    printf("%lld",sum);
+}
