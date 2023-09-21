710b3e644bbd79adf07175d53fdeb34cb75e9c25
ratnamraju-420 <119720469+ratnamraju-420@users.noreply.github.com>
2023-09-21 19:59:47 +0530

    #include<stdio.h> int main() {     int n,arr[100],i,c=0,count=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         if(arr[i]==1)         {             c++;         }         if(arr[i]==0)         {             if(c>count)             {                 count=c;             }             c=0;         }     }     if(c>count)     {         count=c;     }     printf("%d",count); }

---
+++ Max_Consecutive_Ones.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{
+    int n,arr[100],i,c=0,count=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(arr[i]==1)
+        {
+            c++;
+        }
+        if(arr[i]==0)
+        {
+            if(c>count)
+            {
+                count=c;
+            }
+            c=0;
+        }
+    }
+    if(c>count)
+    {
+        count=c;
+    }
+    printf("%d",count);
+}
